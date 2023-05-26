967233c7f1a6cbbb6fbaaa949461bb9ce945d325
이승주(Seungju Lee)/버티컬개발팀/11ST <goodguy@sk.com>
2023-05-26 13:37:29 +0900

    ˆ

--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,3 @@
-from unittest import TestCase
-
-
 import unittest
 
 import lib_calc
@@ -10,33 +7,35 @@ class TddTest(unittest.TestCase):
 
     def testAdd(self):
         result = lib_calc.add(10, 20)
-        if result == 50:
-            print('testAdd OK')
-        else:
-            print('testAdd fail')
+
+        # 결과 값이 일치 여부 확인
+        self.assertEqual(result, 31)
 
     def testSubstract(self):
-        result = lib_calc.substract(20, 30)
+        result = lib_calc.substract(20, 10)
 
-        if result > 0:
+        if result > 10:
             boolval = True
         else:
             boolval = False
 
-        if boolval == False:
-            print('testSubstract Error')
+        # 결과 값이 True 여부 확인
+        self.assertTrue(boolval)
 
     def testDivision(self):
-        try:
-            lib_calc.division(4, 0)
-        except Exception as e:
-            print(e)
+        # 결과 값이 ZeroDivisionError 예외 발생 여부 확인
+        self.assertRaises(ZeroDivisionError, lib_calc.division, 4, 1)
 
     def testMultiply(self):
+        nonechk = True
+
         result = lib_calc.multiply(10, 9)
 
-        if result < 100:
-            print('testMultiply Error')
+        if result > 100:
+            nonechk = None
+
+        # 결과 값이 None 여부 확인
+        self.assertIsNone(nonechk)
 
 if __name__ == '__main__':
     unittest.main()
