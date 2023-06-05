6e2dc5b0d886e02bbbbba6ab4cecd8f2fb47f5c3
yuliepie <yuliekorea@gmail.com>
2023-06-05 23:48:03 +0900

    Migrate first model

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stock_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
