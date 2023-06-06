3c29b99ba6ca17ceb0135238702b7142907450bf
yuliepie <yuliekorea@gmail.com>
2023-06-06 12:03:03 +0900

    Animal API 추가

--- backend/stock_api/admin.py
+++ backend/stock_api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Animal
 
-# Register your models here.
-admin.site.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'owner')  # Specify the fields you want to display
+
+admin.site.register(Animal, AnimalAdmin)
--- backend/stock_api/models.py
+++ backend/stock_api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Animal(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    type = models.CharField(max_length=100, blank=True, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
